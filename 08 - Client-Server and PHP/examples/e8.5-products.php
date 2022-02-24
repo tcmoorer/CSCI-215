c045cf83a86bf518745813610d3637571bad2f43
tcmoorer <97441962+tcmoorer@users.noreply.github.com>
2022-02-24 06:51:11 -0500

    making ids unique :/

--- 08 - Client-Server and PHP/examples/e8.5-products.php
+++ 08 - Client-Server and PHP/examples/e8.5-products.php
@@ -8,21 +8,21 @@ header('Content-Type: application/json');
 // array of product information
 $inventory = [
     [
-        "id" => "c1019e98-491f-11ec-81d3-0242ac130003",
+        "id" => "d05347de-491f-11ec-81d3-0242ac130000",
         "name" => "Apple",
         "inventory" => "1.1,2.0,0.7,1.1",
         "description" => "An apple is an edible fruit produced by an apple tree (Malus domestica). Apple trees are cultivated worldwide and are the most widely grown species in the genus Malus.",
         "file" => "apple.jpg"
     ],
     [
-        "id" => "d05347de-491f-11ec-81d3-0242ac130003",
+        "id" => "d05347de-491f-11ec-81d3-0242ac130001",
         "name" => "Banana",
         "inventory" => "2.1,3.0,4.7,3.1,3.5",
         "description" => "A banana is an elongated, edible fruit – botanically a berry – produced by several kinds of large herbaceous flowering plants in the genus Musa.",
         "file" => "banana.jpg"
     ],
     [
-        "id" => "d05349fa-491f-11ec-81d3-0242ac130003",
+        "id" => "d05349fa-491f-11ec-81d3-0242ac130002",
         "name" => "Cherry",
         "inventory" => "6.1,4.0,2.7,3.1,18.9,3.0",
         "description" => "A cherry is the fruit of many plants of the genus Prunus, and is a fleshy drupe (stone fruit)",
@@ -36,7 +36,7 @@ $inventory = [
         "file" => "grapes.jpg"
     ],
     [
-        "id" => "d0534d4c-491f-11ec-81d3-0242ac130003",
+        "id" => "d0534d4c-491f-11ec-81d3-0242ac130004",
         "name" => "Orange",
         "inventory" => "1.1,2.0,0.7,1.1",
         "description" => "The orange is the fruit of various citrus species in the family Rutaceae (see list of plants known as orange); it primarily refers to Citrus sinensis, which is also called sweet orange, to distinguish it from the related Citrus aurantium, referred to as bitter orange.",
